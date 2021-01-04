055bf12f42545c4ae04bcd0950ad29ff6e1c5af8
dantalmont <dantalmont@gmail.com>
2021-01-03 21:16:35 -0500

    added js for new nav button

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -31,6 +31,14 @@ function Navbar() {
               Projects
             </Link>
           </li>
+          <li className="nav-item">
+            <Link
+              to="/contact"
+              className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}
+            >
+              Contact
+            </Link>
+          </li>
         </ul>
       </div>
     </nav>

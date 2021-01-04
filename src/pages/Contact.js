c6a7dc26d24d0226946463e16aa56a5e94833243
dantalmont <dantalmont@gmail.com>
2021-01-03 21:24:53 -0500

    added contact page for react router

---
+++ src/pages/Contact.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ContactForm from "../components/ContactForm";
+
+function ContactPage() {
+  return (
+    <ContactForm />
+  );
+}
+
+export default ContactPage;

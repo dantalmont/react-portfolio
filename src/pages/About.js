28cdc39d89113b5c00d9d085a5cb3206cd453f72
dantalmont <dantalmont@gmail.com>
2021-01-06 00:36:20 -0500

    about me spacing

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,23 +1,16 @@
 import React from "react";
 import AboutMe from "../components/AboutMe";
-// import { Col , Row , Container } from "react-bootstrap";
+import { Col , Row , Container } from "react-bootstrap";
 // import Image from 'react-bootstrap/Image';
 // import profilPic from '../utils/assets/ProfilePic.jpg'
+import TechIcons from "../components/TechIcons";
 
 function About() {
   return (
-    <AboutMe />
-    // <Container>
-    //   <Row>
-    //     <Col style={{marginTop: '50px'}}><Image src={profilPic} thumbnail /></Col>
-    //     <Col style={{marginTop: '50px'}}>
-    //       <div className="description">
-    //       I am a Full Stack coding student from Springfield, Massachussets. I have been an automotive mechanic for 7 years. 
-    //       </div>
-    //     </Col>
-    //   </Row>
-      
-    // </Container>
+    <div>
+      <AboutMe />
+      <TechIcons />
+    </div>
   );
 }
 

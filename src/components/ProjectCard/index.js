09b48d000e09d0ed28bd2d3eeb5eebec36179577
dantalmont <dantalmont@gmail.com>
2021-01-06 00:34:44 -0500

    added tabs to cards

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,6 +1,24 @@
 import React, { Component } from "react";
 import { Card , Button , Container , Row , Col, Tab, Tabs } from "react-bootstrap";
 import "./style.css";
+import { Icon, InlineIcon } from '@iconify/react';
+import reactIcon from '@iconify-icons/logos/react';
+
+import html5 from '@iconify-icons/logos/html-5';
+import css3 from '@iconify-icons/logos/css-3';
+import javascriptIcon from '@iconify-icons/logos/javascript';
+import mysqlIcon from "@iconify-icons/logos/mysql"
+import mongodbIcon from '@iconify-icons/logos/mongodb';
+import sequelizeIcon from '@iconify-icons/logos/sequelize';
+import firebaseIcon from '@iconify-icons/logos/firebase';
+import materializecssIcon from '@iconify-icons/logos/materializecss';
+import handlebarsIcon from '@iconify-icons/logos/handlebars';
+import gitIcon from '@iconify-icons/logos/git-icon';
+import webpackIcon from '@iconify-icons/logos/webpack';
+import bootstrapIcon from '@iconify-icons/logos/bootstrap';
+import herokuIcon from '@iconify-icons/logos/heroku-icon';
+import nodejsIcon from '@iconify-icons/logos/nodejs';
+import expressIcon from '@iconify-icons/logos/express';
 import repos  from "../../utils/repos.json"
 
 const styles = {
@@ -16,6 +34,7 @@ const styles = {
 }
 
 
+
 class ProjectCard extends Component {
   constructor() {
     super();
@@ -23,21 +42,29 @@ class ProjectCard extends Component {
   }
   render() {
     return repos.map( (projects) =>
-          <Col lg="auto">
+          <Col xs="12" md="6" lg="auto">
             <Card style={{ width: '18rem' }}>
-              <Card.Title style={{textAlign: 'center'}}>{projects.name}</Card.Title>
+              <Card.Title class="cardTitle">{projects.name}</Card.Title>
               <div className="overflow">
-                <Card.Img src={projects.image} style={styles.cardImage} />
+                <Card.Img src={projects.image} className="card-img-top"/>
               </div>
               <Card.Body>
-                <Tabs defaultActiveKey="home" id="uncontrolled-tab-example">
-                  <Tab eventKey="home" title="Description">
-                        <Card.Text style={{ textAlign: 'center' , marginBottom: '15%' }}>
+                <Tabs defaultActiveKey="detail"  className="tabColor">
+                  <Tab eventKey="detail" title="Description">
+                        <Card.Text className="cardText">
                           {projects.description}
                         </Card.Text>
                   </Tab>
-                  <Tab eventKey="profile" title="Technology">
-                    <div>whats up</div>
+                  <Tab eventKey="tech" title="Technology">
+                  <Card.Text className="cardText">{projects.tech}</Card.Text>
+                  <Row>
+                    <Col style={{justifyContent: 'center'}}>
+                      <Button variant="outline-dark" href={projects.demo}>Demo</Button>
+                    </Col>
+                    <Col style={{justifyContent: 'center'}}>
+                      <Button variant="outline-dark" href={projects.repo}>Repository</Button>
+                    </Col>
+                  </Row>
                   </Tab>
                 </Tabs>
               </Card.Body>

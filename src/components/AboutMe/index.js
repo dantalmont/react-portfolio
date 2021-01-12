08c5eb51fc8b5728249facc5bd1041cea38c22c9
dantalmont <dantalmont@gmail.com>
2021-01-12 16:25:39 -0500

    added more styling to about section

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -7,10 +7,14 @@ import "./style.css"
 function AboutMe() {
   return (
     <Container>
-      <Row>
-        <Col xs="8" md="4"><Image src={profilPic} className="proPic" fluid roundedCircle /></Col>
-        <Col xs="8" md="6" >
-          <p className="aboutText"> I am a recent graduate from the full stack program at the University of Connecticut. I have a focus on front end development but appreciate back end and the full stack as a whole. </p>
+      <Row className="justify-content-md-center">
+        <Col xs="2" md="1" />
+       
+        <Col xs="8" md="3"><Image src={profilPic} className="proPic" fluid roundedCircle /></Col>
+        <Col md="1" />
+        <Col xs="11" md="6" style={{backgroundColor: "white", margin: 15, padding: 5, border: 'transparent', borderRadius:'1rem'}}>
+            <p>I am a recent graduate from the full stack program at the University of Connecticut. I have a focus on front end development but appreciate back end and the full stack as a whole.</p>
+            <p>More text here</p>
         </Col>
       </Row>
       

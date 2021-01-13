9a0c9be31d6839491dea0afd66e5acc390851861
dantalmont <dantalmont@gmail.com>
2021-01-13 00:30:20 -0500

    edit summary

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -13,8 +13,7 @@ function AboutMe() {
         <Col xs="8" md="3"><Image src={profilPic} className="proPic" fluid roundedCircle /></Col>
         <Col md="1" />
         <Col xs="11" md="6" style={{backgroundColor: "white", margin: 15, padding: 5, border: 'transparent', borderRadius:'1rem'}}>
-            <p>I am a recent graduate from the full stack program at the University of Connecticut. I have a focus on front end development but appreciate back end and the full stack as a whole.</p>
-            <p>More text here</p>
+            <p>Full Stack Web Developer with a background in Automotive. I am a recent graduate from the Full Stack program at the University of Connecticut. Able to build websites start to finish. The Automotive industry has provided me with many useful skills including being adaptive and keeping up with current technology, detail oriented, and deadline driven.</p>
         </Col>
       </Row>
       

388773ce536f72cde24197e2fff55924183754b6
dantalmont <dantalmont@gmail.com>
2021-01-14 16:25:13 -0500

    updated resume link

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,7 +11,7 @@ function Footer() {
           <SocialIcon url="https://github.com/dantalmont" style={{ height: 25, width: 25, marginRight: '5px' }} />
           <SocialIcon url="https://www.linkedin.com/in/danieltalmont" style={{ height: 25, width: 25, marginRight: '5px' }} />
           <SocialIcon url="https://www.facebook.com/Dantalmont" style={{ height: 25, width: 25, marginRight: '5px' }} />
-          <SocialIcon url="https://docs.google.com/document/d/1vYd3viY-0DGhTxThAYPZS7K6dosuaJ1hN4YXvLf0N-E/edit?usp=sharing" label="My resume" style={{ height: 25, width: 25, marginRight: '5px' }} />
+          <SocialIcon url="http://bit.ly/danieltalmontresume" label="My resume" style={{ height: 25, width: 25, marginRight: '5px' }} />
         </div>
         Daniel Talmont &copy;2020
       </span>
